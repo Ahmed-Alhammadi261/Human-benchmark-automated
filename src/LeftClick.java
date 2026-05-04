1a48e7dddd497c1f033f909b4712644899fada78
Ahmed-Alhammadi261 <alhammadiahmed261@gmail.com>
2026-05-04 22:14:40 +0400

    Rename mouseLeftClick method to click

--- src/LeftClick.java
+++ src/LeftClick.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.InputEvent;
 
 public class LeftClick {
-    public static void mouseLeftClick(Robot mouse) {
+    public static void click(Robot mouse) {
 
         mouse.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         mouse.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
