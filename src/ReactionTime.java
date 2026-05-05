3645183426874d867c408fb7e22b7ad646e6de2e
Ahmed-Alhammadi261 <alhammadiahmed261@gmail.com>
2026-05-05 15:43:56 +0400

    Refactor mouse control to use MouseControler class

--- src/ReactionTime.java
+++ src/ReactionTime.java
@@ -12,8 +12,8 @@ public class ReactionTime {
             return;
         }
 
-        mouseBot.mouseMove(POSX, POSY);
-        LeftClick.click(mouseBot);
+        MouseControler.move(mouseBot, POSX, POSY);
+        MouseControler.click(mouseBot);
 
         Color targetColor = new Color(75, 219, 106);
         Color pixelColor;
@@ -24,7 +24,7 @@ public class ReactionTime {
             pixelColor = mouseBot.getPixelColor(POSX, POSY);
 
             if (pixelColor.equals(targetColor)) {
-                LeftClick.click(mouseBot);
+                MouseControler.click(mouseBot);
 
                 isDone = true;
             }
